654e297537997f259c6d494c7dccc88510a5222d
ThinhPTran <trphthinh@gmail.com>
2017-07-11 15:31:41 +0700

    A look at data.

--- src/clj/preparedatomicdb/db.clj
+++ src/clj/preparedatomicdb/db.clj
@@ -12,16 +12,74 @@
 (def persist-atom (durable-atom "/home/appsmith/Datastore/glue27wells.dat"))
 
 ;;;
+(defn getDsnList []
+  (->> @persist-atom
+       (:wells)
+       (map #(:dsn %))
+       (distinct)
+       (vec)))
+
+(defn getWellNameList []
+  (->> @persist-atom
+       (:wells)
+       (map #(:well %))
+       (vec)))
+
+(def currentdsn :petronasdb)
+(def currentwell {:field "BOKOR"
+                  :lease "DP-A"
+                  :well "A114"
+                  :cmpl "SS"})
+
+(defn getWellNameListOfADataSourceName [currentdsn]
+  (->> @persist-atom
+       (:wells)
+       (filter #(= (first (keys (:dsn %))) currentdsn))
+       (map #(:well %))
+       (vec)))
+
+(defn getAWell [dsn well]
+  (->> @persist-atom
+       (:wells)
+       (filter #(and (= dsn (first (keys (:dsn %))))
+                     (= well (:well %))))
+       (first)
+       (:welldoc)))
+
+(defn getAllkeysAWell [dsn well]
+  (->> @persist-atom
+       (:wells)
+       (filter #(and (= dsn (first (keys (:dsn %))))
+                     (= well (:well %))))
+       (first)
+       (keys)
+       ))
+
+(defn getAllkeysWelldocAWell [dsn well]
+  (->> @persist-atom
+       (:wells)
+       (filter #(and (= dsn (first (keys (:dsn %))))
+                     (= well (:well %))))
+       (first)
+       (:welldoc)
+       (keys)
+       ))
+
 (defn getAllWells
   "Get all wells in the datastore"
   []
   (->> @persist-atom
        (:wells)))
 
+;;; Init schema for well
 
 (defn initdb []
   (println "Get data from durable-atom to check")
-  (pp/pprint (getAllWells)))
+  (pp/pprint (getDsnList))
+  (pp/pprint (getWellNameList))
+  (pp/pprint (getWellNameListOfADataSourceName currentdsn))
+  (pp/pprint (getAllkeysAWell currentdsn currentwell))
+  (pp/pprint (getAllkeysWelldocAWell currentdsn currentwell)))
 
 
 
